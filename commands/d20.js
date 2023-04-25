4092292871c021dfc9e67739ed291a19ca10e98b
Mathieu Stephan <matsteph56@gmail.com>
2023-04-25 14:54:29 +0200

    i forgor

--- commands/d20.js
+++ commands/d20.js
@@ -33,7 +33,7 @@ module.exports = {
         //add text : result in the middle
         ctx.font = size + ' sans-serif'
         ctx.fillStyle = '#ffffff'
-        ctx.fillText(result, 900, 1200)
+        ctx.fillText(result, location, 1200)
         const body = await interaction.user.displayAvatarURL({ extension: 'jpg' })
         const avatar = await loadImage(body)
         //add rounded avatar at the top right

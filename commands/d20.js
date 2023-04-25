7bebc909ed9956875827fdba7e02edc83e32b7c5
Mathieu Stephan <matsteph56@gmail.com>
2023-04-25 14:02:23 +0200

    adding d20

---
+++ commands/d20.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('discord.js')
+const Discord = require('discord.js')
+const { EmbedBuilder } = require('discord.js');
+const { createCanvas, loadImage, encode } = require('canvas')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('d20')
+        .setDescription('Faite un jet de dé 20'),
+    async execute(interaction) {
+        let result = Math.floor(Math.random() * 20) + 1;
+        let answer = ""
+        let color = "#0099ff"
+        if(result == 1){ 
+            answer = "Vous avez fait un 1, c'est un échec critique !"
+            color = "#ff0000"
+
+        }
+        else if(result == 20) {
+            answer = "Vous avez fait un 20, c'est une réussite critique !"
+            color = "#00ff00"
+        }
+        else answer = `Vous avez fait un ${result}`
+        
+        const embed = new Discord.EmbedBuilder()
+            .setColor(color)
+            .setTitle('Jet de dé 20')
+            .setDescription(`Vous avez fait un ${result}`)
+            .setTimestamp()
+        await interaction.reply({ embeds: [embed] });
+    }
+}

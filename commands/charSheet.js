f2dd1a589c576d9f57f12977ee820b045993d69e
Mathieu Stephan <matsteph56@gmail.com>
2023-04-26 10:56:04 +0200

    adding charSheet

---
+++ commands/charSheet.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js')
+const Discord = require('discord.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('charSheet')
+        .setDescription('Affiche la fiche de personnage'),
+    async execute(interaction) {
+        //get data from charSheets.json
+        const fs = require('fs');
+        const charSheets = JSON.parse(fs.readFileSync('./charSheets.json', 'utf8'));
+        //get the character sheet of the user inside data
+        const charSheet = charSheets[interaction.user.id];
+        //get the name of the character
+        const charName = charSheet.name;
+        //create the embed
+        interaction.reply({ charName});
+
+    }
+}
+
+

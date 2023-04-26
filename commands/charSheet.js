362e654b817aceba54edfcc6d6c4497ab252a2dc
Mathieu Stephan <matsteph56@gmail.com>
2023-04-26 11:16:44 +0200

    update commands

--- commands/charSheet.js
+++ commands/charSheet.js
@@ -11,10 +11,26 @@ module.exports = {
         const charSheets = JSON.parse(fs.readFileSync('./charSheets.json', 'utf8'));
         //get the character sheet of the user inside data
         const charSheet = charSheets[interaction.user.id];
-        //get the name of the character
-        const charName = charSheet.name;
         //create the embed
-        interaction.reply({ charName});
+        const embed = new Discord.EmbedBuilder()
+            .setColor('#0099ff')
+            .setAuthor({ name: 'DiceWizard', iconURL: 'https://cdn.discordapp.com/app-icons/1100141622328557688/df91a59c63c429e963031a5af7d8a165.png?size=256' })
+            .setTitle('Fiche de personnage')
+            .setDescription('Voici votre fiche de personnage')
+            .addFields(
+                { name: 'Nom', value: charSheet.name },
+                { name: 'Classe', value: charSheet.class },
+                { name: 'Niveau', value: charSheet.level },
+                { name: 'Sorts', value: charSheet.spells },
+                { name: 'Compétences', value: charSheet.skills },
+                { name: 'Caractéristiques', value: charSheet.stats }
+            )
+            .setFooter({ text: 'DiceWizard V0.1' })
+            .setTimestamp()
+        //send the embed
+        await interaction.reply({ embeds: [embed] });
+
+        
 
     }
 }

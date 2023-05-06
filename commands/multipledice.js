643089e43efe59be98457e98e7cec946050a1de2
Mathieu <matsteph56@gmail.com>
2023-05-06 11:29:05 +0200

    fixing multiple dice

--- commands/multipledice.js
+++ commands/multipledice.js
@@ -26,61 +26,68 @@ module.exports = {
         ),
     async execute(interaction) {
         results = [];
+        
+        numberOfDice = interaction.options.getString('nombre-dés');
+        numberOfDice = parseInt(numberOfDice);
+        diceType = interaction.options.getString('type-dés');
+
+
         //check if 'nombre-dés' is a number
-        if(isNaN(interaction.options.getString('nombre-dés'))){
+        if(isNaN(numberOfDice)){
             await interaction.reply({ content: 'Le nombre de dés doit être un nombre !', ephemeral: true });
             return;
         }
 
         //check if the number of dice is valid
-        if(interaction.options.getString('nombre-dés') < 1){
-            await interaction.reply({ content: 'Vous ne pouvez pas lancer moins d\'un dé !', ephemeral: true });
+        if(numberOfDice < 1){
+            await interaction.reply({ content: 'Vous devez lancer au moins un dé !', ephemeral: true });
             return;
         }
 
-        if(interaction.options.getString('nombre de dés') > 100){
-            await interaction.reply({ content: 'Vous ne pouvez pas lancer plus de 100 dés à la fois !', ephemeral: true });
+        if(numberOfDice > 100){
+            await interaction.reply({ content: 'Vous ne pouvez pas lancer plus de 100 dés !', ephemeral: true });
             return;
         }
 
         // if 'type-dés' is d4
-        if(interaction.options.getString('type-dés') == 'd4'){
-            for(i = 0; i < interaction.options.getString('nombre-dés'); i++){
+        if(diceType == 'd4'){
+            for(i = 0; i < numberOfDice; i++){
                 results.push(Math.floor(Math.random() * 4) + 1);
             }
         }
 
         // if 'type-dés' is d6
-        if(interaction.options.getString('type-dés') == 'd6'){
-            for(i = 0; i < interaction.options.getString('nombre-dés'); i++){
+        if(diceType == 'd6'){
+            for(i = 0; i < numberOfDice; i++){
                 results.push(Math.floor(Math.random() * 6) + 1);
             }
         }
 
         // if 'type-dés' is d8
-        if(interaction.options.getString('type-dés') == 'd8'){
-            for(i = 0; i < interaction.options.getString('nombre-dés'); i++){
+        if(diceType == 'd8'){
+            for(i = 0; i < numberOfDice; i++){
                 results.push(Math.floor(Math.random() * 8) + 1);
             }
         }
 
+
         // if 'type-dés' is d10
-        if(interaction.options.getString('type-dés') == 'd10'){
-            for(i = 0; i < interaction.options.getString('nombre-dés'); i++){
+        if(diceType == 'd10'){
+            for(i = 0; i < numberOfDice; i++){
                 results.push(Math.floor(Math.random() * 10) + 1);
             }
         }
 
         // if 'type-dés' is d12
-        if(interaction.options.getString('type-dés') == 'd12'){
-            for(i = 0; i < interaction.options.getString('nombre-dés'); i++){
+        if(diceType == 'd12'){
+            for(i = 0; i < numberOfDice; i++){
                 results.push(Math.floor(Math.random() * 12) + 1);
             }
         }
 
         // if 'type-dés' is d20
-        if(interaction.options.getString('type-dés') == 'd20'){
-            for(i = 0; i < interaction.options.getString('nombre-dés'); i++){
+        if(diceType == 'd20'){
+            for(i = 0; i < numberOfDice; i++){
                 results.push(Math.floor(Math.random() * 20) + 1);
             }
         }

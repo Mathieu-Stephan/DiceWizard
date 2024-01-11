0199a7f3230698f0e6a8e6c371160230b0c48c9b
Mathieu Stephan <matsteph56@gmail.com>
2024-01-11 11:28:58 +0100

    debug charsheet

--- commands/charModif.js
+++ commands/charModif.js
@@ -58,7 +58,7 @@ module.exports = {
                 "stats":{"Force":0,"Dextérité":0,"Constitution":0,"Intelligence":0,"Sagesse":0,"Charisme":0},
                 "spells":[""]
             }
-            console.log("test" + charSheets);
+            console.log("test" + JSON.stringify(charSheets));
             await interaction.reply({ content: 'L\'utilisateur n\'avait pas de fiche de personnage, il a donc été ajouté !', ephemeral: true });
             return;
         }
